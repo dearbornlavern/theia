4db7dada998c3ce52890b7cb43af71ebde78386a
Igor Vinokur <ivinokur@redhat.com>
2020-05-12 11:02:42 +0300

    fixup! Select created new files and folders in the navigator

--- packages/navigator/src/browser/navigator-model.spec.ts
+++ packages/navigator/src/browser/navigator-model.spec.ts
@@ -136,6 +136,7 @@ describe('FileNavigatorModel', () => {
     const mockFileChangeEmitter: Emitter<FileChange[]> = new Emitter();
     const mockFileMoveEmitter: Emitter<FileMoveEvent> = new Emitter();
     const mockTreeChangeEmitter: Emitter<void> = new Emitter();
+    const mockNodesAddedEmitter: Emitter<TreeNode[]> = new Emitter();
     const mockExpansionChangeEmitter: Emitter<Readonly<ExpandableTreeNode>> = new Emitter();
 
     let navigatorModel: FileNavigatorModel;
@@ -188,6 +189,7 @@ describe('FileNavigatorModel', () => {
         sinon.stub(mockFileSystemWatcher, 'onFilesChanged').value(mockFileChangeEmitter.event);
         sinon.stub(mockFileSystemWatcher, 'onDidMove').value(mockFileMoveEmitter.event);
         sinon.stub(mockFileNavigatorTree, 'onChanged').value(mockTreeChangeEmitter.event);
+        sinon.stub(mockFileNavigatorTree, 'onNodesAdded').value(mockNodesAddedEmitter.event);
         sinon.stub(mockFileNavigatorTree, 'onDidChangeBusy').value(Event.None);
         sinon.stub(mockTreeExpansionService, 'onExpansionChanged').value(mockExpansionChangeEmitter.event);
 
